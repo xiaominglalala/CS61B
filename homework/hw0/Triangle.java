db51a60a2d9760a8cbd3fee23c6a421184294d8c
xiaominglalala <208436707@qq.com>
2019-07-01 11:47:18 +0800

    None,just try

---
+++ homework/hw0/Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle
+{
+	public static void main(String[] args)
+	{
+		int i = 0;
+		String star = "*";
+		for(i = 1;i < 6;i++)
+		{
+			System.out.println(star);
+			star = star + "*";
+		}
+	}
+}
